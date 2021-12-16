fd513b09be2ae514b10264a94e17a98981ec064b
sivaphani2003 <79915101+sivaphani2003@users.noreply.github.com>
2021-12-16 19:20:07 +0530

    Create recursive polynomial merging sll.c

---
+++ recursive polynomial merging sll.c
@@ -0,0 +1,25 @@
+SinglyLinkedListNode* mergeLists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    SinglyLinkedListNode *p = head1;
+    SinglyLinkedListNode *q = head2;
+    SinglyLinkedListNode *r = head1;
+    if(head1==NULL)
+    {
+        return head2;
+    }
+    else if(head2==NULL)
+    {
+        return head1;
+    }
+
+    if (head1->data <= head2->data)  
+    {  
+        r = head1;  
+        r->next = mergeLists(head1->next, head2);  
+    }  
+    else
+    {  
+        r = head2;  
+        r->next = mergeLists(head1, head2->next);  
+    }  
+    return r;
+}
