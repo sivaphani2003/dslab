ccef3b7e980ea8d92edf44edead8ecc0cc4f5bef
sivaphani2003 <79915101+sivaphani2003@users.noreply.github.com>
2021-12-28 09:37:12 +0530

    Update listoperations in sll.c

--- listoperations in sll.c
+++ listoperations in sll.c
@@ -29,15 +29,22 @@ void sort(node *first)
         new=(node*)malloc(sizeof(node));
         new->data=0;
         new->next=NULL;
-        for(temp1=first;temp1->next!=NULL;temp1=temp1->next)
+        if(first==NULL)
         {
-                for(temp2->next=first;temp2!=NULL;temp2=temp2->next)
+                printf("there is no list to sort");
+        }
+        else
+        {
+                for(temp1=first;temp1->next!=NULL;temp1=temp1->next)
                 {
-                        if(temp1->data > temp2->data)
+                        for(temp2=temp1->next;temp2!=NULL;temp2=temp2->next)
                         {
-                                new->data = temp1->data;
-                                temp1->data = temp2->data;
-                                temp2->data=new->data;
+                                if(temp1->data > temp2->data)
+				{
+                                        new->data = temp1->data;
+                                        temp1->data = temp2->data;
+                                        temp2->data=new->data;
+                                }
                         }
                 }
         }
@@ -57,7 +64,8 @@ node* create(node *first)
                 {
                         first=new;
                 }
-                else{
+                else
+                {
                         prev->next=new;
                 }
                 prev=new;
@@ -88,13 +96,9 @@ node * concatenate(node* l1,node *l2)
         node *l3=NULL;
         node *temp;
         if(l1==NULL)
-        {
                 l3=l2;
-        }
         else if (l2==NULL)
-        {
                 l3=l1;
-        }
         else
         {
                 node *new;
@@ -113,9 +117,8 @@ node * concatenate(node* l1,node *l2)
                         temp=temp->next;
                 }
                 temp->next=l2;
-          
-       }
-      return l3;
+                return l3;
+        }
 }
 node * listunion(node *l1,node *l2)
 {
@@ -140,8 +143,8 @@ node * listunion(node *l1,node *l2)
                 }
                 else
                 {
-                   new=(node*)malloc(sizeof(node));
-                        new->data=l1->data;
+                        new=(node*)malloc(sizeof(node));
+                        new->data=l2->data;
                         new->next=NULL;
                         l3=addnode(l3,new);
                         l1=l1->next;
@@ -174,7 +177,7 @@ node * listunion(node *l1,node *l2)
 }
 node * listmerge(node *l1,node *l2)
 {
-        node *l3=NULL,*new;
+        node *l3=NULL,*new,*new1,*new2;
         while(l1!=NULL && l2!=NULL)
         {
                 if(l1->data<l2->data)
@@ -195,11 +198,12 @@ node * listmerge(node *l1,node *l2)
                 }
                 else
                 {
-                        new=(node*)malloc(sizeof(node));
-                        new->data=l1->data;
-                        new->next=NULL;
-                        l3=addnode(l3,new);
-                        l3=addnode(l3,new);
+                        new1=(node*)malloc(sizeof(node));
+			new2=(node*)malloc(sizeof(node));
+                        new1->data=new2->data=l1->data;
+                        new1->next=new2->next=NULL;
+                        l3=addnode(l3,new1);
+                        l3=addnode(l3,new2);
                         l1=l1->next;
                         l2=l2->next;
                 }
@@ -228,13 +232,68 @@ node * listmerge(node *l1,node *l2)
         }
         return l3;
 }
+node *listintersection(node*l1,node*l2)
+{
+        node *l3=NULL;
+        node *new;
+        while(l1!=NULL && l2!=NULL)
+        {
+                if(l1->data <l2->data)
+                {
+                        l1=l1->next;
+                }
+                else if (l1->data>l2->data)
+                {
+                        l2=l2->next;
+                }
+                else
+                {
+                        new=(node *)malloc(sizeof(node));
+                        new->data=l1->data;
+                        new->next=NULL;
+                        l3=addnode(l3,new);
+                        l1=l1->next;
+                        l2=l2->next;
+                }
+        }
+        return l3;
+}
 void main()
 {
-        node *l1,*l2,*l3,*head=NULL;
-        l1=create(head);
-        l2=create(head);
+        int ch;
+        node *l1=NULL,*l2=NULL,*l3;
+        l1=create(l1);
+        display(l1);
+        l2=create(l2);
+        display(l2);
+        printf("after sortig the lists");
+        sort(l1);
+        sort(l2);
         display(l1);
         display(l2);
-        l3=concatenate(l1,l2);
-        display(l3);
+        while(1)
+        {
+                printf("enter your choice\n");
+                printf("1:concatenate\n2:union\n3:intersection\n4:merge\n5:exit\n");
+                scanf("%d",&ch);
+                switch(ch)
+                {
+                        case 1: l3=concatenate(l1,l2);
+                                display(l3);
+                                break;
+                        case 2: l3=listunion(l1,l2);
+                                display(l3);
+                                break;
+                        case 3: l3=listintersection(l1,l2);
+                                display(l3);
+                                break;
+                        case 4: l3=listmerge(l1,l2);
+                                display(l3);
+                                break;
+                        case 5: exit(0);
+                        default:printf("enter the correct choice ");
+                }
+        }
+
+
 }
